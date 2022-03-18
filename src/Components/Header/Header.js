6cd2e869399fadb6f90b202383c61ad9d1405396
siam3371 <siamhossain3371@gmail.com>
2022-03-18 19:00:51 +0600

    i back my position

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,26 +6,26 @@ const Header = () => {
         <nav className="navbar navbar-expand-lg background-navbar  text-success   fs-5">
         <div className="container-fluid">
 
-        <h2 className="navbar-brand fw-bold fs-3 ">Maruf Hossain Siam</h2>
+        <h2 className="navbar-brand fw-bold fs-3">Maruf Hossain Siam</h2>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
            <div className="collapse navbar-collapse " id="navbarTogglerDemo03">
-            <ul className="navbar-nav ms-auto mb-lg-0 d-flex justify-content-end ">
+            <ul className="navbar-nav ms-auto mb-lg-0 d-flex justify-content-end">
               <li className="nav-item">
-                <NavLink className="nav-link fw-bold"  to="/home" aria-current="page" >Home</NavLink>
+                <NavLink className="nav-link fw-bold" to="/home" aria-current="page" >Home</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link fw-bold"  to="/projects"  aria-current="page"  >Projects</NavLink>
+                <NavLink className="nav-link fw-bold" to="/projects"  aria-current="page"  >Projects</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link fw-bold"  to="/about"  aria-current="page"  >About</NavLink>
+                <NavLink className="nav-link fw-bold" to="/about"  aria-current="page"  >About</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link fw-bold"  to="/blogs"  aria-current="page"  >Blogs</NavLink>
+                <NavLink className="nav-link fw-bold" to="/blogs"  aria-current="page"  >Blogs</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link fw-bold"   to="/contact" aria-current="page"  >Contact Me</NavLink>
+                <NavLink className="nav-link fw-bold"  to="/contact" aria-current="page"  >Contact Me</NavLink>
               </li>
             </ul> 
           </div>
